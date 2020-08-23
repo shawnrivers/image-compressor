31ab8c09d14eddd26dfa8d216875105f36fe1a87
Shawn Rivers <kaushou@gmail.com>
2020-08-23 22:14:58 +0900

    Pass resize option to resize function

--- src/resize.ts
+++ src/resize.ts
@@ -1,21 +1,28 @@
 import sharp from 'sharp';
 import { getPath } from './file';
 
-export const resize = async (imagePath: string): Promise<string> => {
+export const resize = async (
+  imagePath: string,
+  resizeRate?: number
+): Promise<string> => {
   const { filename, dirname, extension } = getPath(imagePath);
+
+  console.log(resizeRate);
 
   try {
     const metaData = await sharp(imagePath).metadata();
     const { width, height } = metaData;
 
     if (width !== undefined && height !== undefined) {
-      let resizeRate = 1;
-      const size = Math.min(width, height);
+      if (resizeRate === undefined) {
+        resizeRate = 1;
+        const size = Math.min(width, height);
 
-      for (let i = 1; i < 10; i++) {
-        if (size / i <= 2000 && size / i >= 1000) {
-          resizeRate = i;
-          break;
+        for (let i = 1; i < 10; i++) {
+          if (size / i <= 2000 && size / i >= 1000) {
+            resizeRate = i;
+            break;
+          }
         }
       }
 
